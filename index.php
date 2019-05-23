4ac80373abee60e4a89d46a96f8e056988eeee6f
danielzarazua <danielzarazuaflores@hotmail.com>
2019-05-23 10:48:56 -0500

    UPDATE:archivo precios

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
 		<meta charset="utf-8">
 		<script src="/examen2/js/jquery-3.3.1.js"></script>
 		<script src="/examen2/js/ajax_post.js"></script>
+		<script src="/examen2/precios.html"></script>
+		
 
 	</head>
 
 	<body>
 		<button>Presentacion libro</button>
+		<button>ver precios</button>
 	</body>
 </html>
